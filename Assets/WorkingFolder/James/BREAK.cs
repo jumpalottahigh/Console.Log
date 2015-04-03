38e820b4a2929840da967bfa234645a89a3f1cc5
Entropicgoat <james.narraway@btinternet.com>
2015-04-03 15:20:46 +0300

    Testing Github
    Should still be 0.0 but we already have 1 for some reason.

---
+++ Assets/WorkingFolder/James/BREAK.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BREAK : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("TEST");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
