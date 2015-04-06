3d0fe7310730bf4f3352855fba3b93ca8fc097fc
Georgi Yanev <georgiyanev.gy@gmail.com>
2015-04-06 17:55:16 +0300

    2.2
    Circuit Enemy is In
    Console 3d model is in
    simple door opening/closing script is in
    added textures
    level 1 design complete(need to put in elevator to level2)

---
+++ Assets/Scripts/Environment/door1_open.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class door1_open : MonoBehaviour {
+
+	public bool locked = true;
+	public bool playerNear = false;
+
+	public Transform[] doorTransforms;
+
+	public Transform door_left;
+	public Transform door_right;
+	public Transform leftEnd;
+	public Transform rightEnd;
+	public Transform leftStart;
+	public Transform rightStart;
+
+	// Use this for initialization
+	void Start () {
+		doorTransforms = this.GetComponentsInChildren<Transform>();
+		door_left = doorTransforms[1];
+		door_right = doorTransforms[2];
+		leftEnd = doorTransforms [3];
+		rightEnd = doorTransforms [4];
+		leftStart = doorTransforms [5];
+		rightStart = doorTransforms [6];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!locked) {
+			if (playerNear) {
+				door_left.position = Vector3.Lerp (door_left.position, leftEnd.position, Time.deltaTime);
+				door_right.position = Vector3.Lerp (door_right.position, rightEnd.position, Time.deltaTime);
+			} else {
+				door_left.position = Vector3.Lerp (door_left.position, leftStart.position, Time.deltaTime);
+				door_right.position = Vector3.Lerp (door_right.position, rightStart.position, Time.deltaTime);
+			}
+		}
+	}
+
+	void OnTriggerEnter(Collider other){
+		if (other.CompareTag("Player")){
+			Debug.Log("Player enter");
+			playerNear = true;
+		}
+
+	}
+
+	void OnTriggerExit(Collider other) {
+		if (other.CompareTag("Player")){
+			Debug.Log("Player LEFT");
+			playerNear = false;
+		}
+	}
+
+
+}
