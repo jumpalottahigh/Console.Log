74f16014eca68f934ab74cff90039a5fa67c90e4
Entropicgoat <james.narraway@btinternet.com>
2015-04-08 17:57:21 +0300

    2.9
    Added more console commands (elevator speed code will be redone) and
    text autoscrolling.

--- Assets/WorkingFolder/James/consoleScript.cs
+++ Assets/WorkingFolder/James/consoleScript.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Text;
 
 public class consoleScript : MonoBehaviour {
 
@@ -13,7 +14,9 @@ public class consoleScript : MonoBehaviour {
 	private string textOutput = "";
 	private string mode = "Basic";
 	private string modeOptions = "";
+	private string defaultOptions = "";
 	private float cooldown = 2;
+	private int numlines, maxlines, eleSpeed;
 
 	// Detects what is child of the console and what options it should have
 	void Start () {
@@ -34,6 +37,7 @@ public class consoleScript : MonoBehaviour {
 				modeOptions = modeOptions + "Light\n";
 			}
 		}
+		defaultOptions = modeOptions;
 	}
 	// Update is called once per frame
 	void Update () {
@@ -53,7 +57,11 @@ public class consoleScript : MonoBehaviour {
 
 	private void inputFunc(){
 		textInput = textInput.ToLower ();
-
+		numlines = consoleStr.Split ('\n').Length;
+		maxlines = (Screen.height - 52) / 17;
+		if (numlines > maxlines) {
+			consoleStr = consoleStr.Substring (consoleStr.IndexOf ('\n') + 1);
+		}
 		//Basic level input check
 		if (mode == "Basic") {
 			if (textInput == "?") {
@@ -72,69 +80,135 @@ public class consoleScript : MonoBehaviour {
 				mode = mode + "/Elevator";
 				modeOptions = "Power\nSpeed\nExit\n";
 				textOutput = "";
-			} else{
+				spamshield = true;
+			} else {
 				textOutput = "Unrecognised command\n";
 			}
 		}
 
 		//Door level input check
 		if (mode == "Basic/Door") {
-			if (textInput == "?"){
+			if (textInput == "?") {
 				textOutput = modeOptions;
-			} else if (textInput == "exit"){
+			} else if (textInput == "exit") {
 				mode = "Basic";
 				textOutput = "";
+				modeOptions = defaultOptions;
 				spamshield = true;
-			} else if (textInput == "power" || textInput == "lock"){
+			} else if (textInput == "power" || textInput == "lock") {
 				textOutput = "Incomplete command\n";
-			} else if (textInput == "power ?" || textInput == "lock ?"){
+			} else if (textInput == "power ?" || textInput == "lock ?") {
 				textOutput = "On\nOff\n";
-			} else if (textInput == "power on"){
+			} else if (textInput == "power on") {
 				textOutput = "Door is now powered\n";
-			} else if (textInput == "power off"){
+			} else if (textInput == "power off") {
 				textOutput = "Door is no longer powered\n";
-			} else if (textInput == "lock on"){
+			} else if (textInput == "lock on") {
 				textOutput = "Door is now locked\n";
-			} else if (textInput == "lock off"){
+			} else if (textInput == "lock off") {
 				textOutput = "Door is now unlocked\n";
-			} else if (spamshield == true){
+			} else if (spamshield == true) {
 				textOutput = "";
 				spamshield = false;
-			}else {
+			} else {
 				textOutput = "Unrecognised command\n";
 			}
 		}
 
 		//Light level input check
 		if (mode == "Basic/Light") {
-			if (textInput == "?"){
+			if (textInput == "?") {
 				textOutput = modeOptions;
-			} else if (textInput == "exit"){
+			} else if (textInput == "exit") {
 				mode = "Basic";
 				textOutput = "";
+				modeOptions = defaultOptions;
 				spamshield = true;
-			} else if (textInput == "power" || textInput == "color"){
+			} else if (textInput == "power" || textInput == "color") {
 				textOutput = "Incomplete command\n";
-			} else if (textInput == "power ?"){
+			} else if (textInput == "power ?") {
 				textOutput = "On\nOff\nSexy\n";
-			} else if (textInput == "color ?"){
+			} else if (textInput == "color ?") {
 				textOutput = "White\nGreen\nSexy\n";
-			} else if (textInput == "power on"){
+			} else if (textInput == "power on") {
 				textOutput = "Lights are powered on\n";
-			} else if (textInput == "power off"){
+			} else if (textInput == "power off") {
 				textOutput = "Lights are powered off\n";
-			} else if (textInput == "power sexy"){
+			} else if (textInput == "power sexy") {
 				textOutput = "Lights are set to SEXY!\n";
-			} else if (textInput == "color white"){
+			} else if (textInput == "color white") {
 				textOutput = "Light color set to white";
-			} else if (textInput == "color green"){
+			} else if (textInput == "color green") {
 				textOutput = "Light color set to green";
-			} else if (textInput == "color sexy"){
+			} else if (textInput == "color sexy") {
 				textOutput = "BOOM BABY SEXY LIGHTS!\n";
-			} else if (spamshield == true){
+			} else if (spamshield == true) {
 				textOutput = "";
 				spamshield = false;
-			}else {
+			} else {
+				textOutput = "Unrecognised command\n";
+			}
+		}
+
+		//Elevator level input check
+		if (mode == "Basic/Elevator") {
+			if (textInput == "?") {
+				textOutput = modeOptions;
+			} else if (textInput == "exit") {
+				mode = "Basic";
+				textOutput = "";
+				modeOptions = defaultOptions;
+				spamshield = true;
+			} else if (textInput == "power" || textInput == "speed") {
+				textOutput = "Incomplete command\n";
+			} else if (textInput == "power ?") {
+				textOutput = "On\nOff\n";
+			} else if (textInput == "speed ?") {
+				textOutput = "speed [1-10]\n";
+			} else if (textInput == "power on") {
+				textOutput = "Elevator is powered on\n";
+			} else if (textInput == "power off") {
+				textOutput = "Elevator is powered off\n";
+			/*This code needs to be cleaned up if I can check if a string has a set part and then
+			a wildcard part. Will use the following code to get the int value out of the string
+			for (int i = 0; i < textInput.Length; i++){
+				if (Char.IsDigit(textOutput[i]))
+				eleSpeed = textOutput[i]
+			}*/
+			} else if (textInput == "speed 1"){
+				eleSpeed = 1;
+				textOutput = "Elevator speed set to 1\n";
+			} else if (textInput == "speed 2"){
+				eleSpeed = 2;
+				textOutput = "Elevator speed set to 2\n";
+			} else if (textInput == "speed 3"){
+				eleSpeed = 3;
+				textOutput = "Elevator speed set to 3\n";
+			} else if (textInput == "speed 4"){
+				eleSpeed = 4;
+				textOutput = "Elevator speed set to 4\n";
+			} else if (textInput == "speed 5"){
+				eleSpeed = 5;
+				textOutput = "Elevator speed set to 5\n";
+			} else if (textInput == "speed 6"){
+				eleSpeed = 6;
+				textOutput = "Elevator speed set to 6\n";
+			} else if (textInput == "speed 7"){
+				eleSpeed = 7;
+				textOutput = "Elevator speed set to 7\n";
+			} else if (textInput == "speed 8"){
+				eleSpeed = 8;
+				textOutput = "Elevator speed set to 8\n";
+			} else if (textInput == "speed 9"){
+				eleSpeed = 9;
+				textOutput = "Elevator speed set to 9\n";
+			} else if (textInput == "speed 10"){
+				eleSpeed = 10;
+				textOutput = "Elevator speed set to 10\n";
+			}else if (spamshield == true) {
+				textOutput = "";
+				spamshield = false;
+			} else {
 				textOutput = "Unrecognised command\n";
 			}
 		}
